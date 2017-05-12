d81dc2ac391e61064386d9165ea3dcf8faa33c54
Raimundo Herrera <rjherrera@uc.cl>
2017-05-12 02:05:25 -0300

    getting ready for deployment

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn raimundoherrera.wsgi
+
--- raimundoherrera/settings.py
+++ raimundoherrera/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p=rg7u_x4e=y7v^$ht%k=3p6ii%)tn9p5oc4hz9+l#@s+o%xr2'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['raimundoherrera.com', 'www.raimundoherrera.com',
+                 '127.0.0.1', 'localhost', '[::1]']
 
 
 # Application definition
