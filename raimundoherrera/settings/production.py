45682bc3755949fdba232c4a7e0527d20458e060
Raimundo Herrera <rjherrera@uc.cl>
2021-02-07 01:30:51 -0300

    chore(static): add whitenoise for production static serving

--- raimundoherrera/settings/production.py
+++ raimundoherrera/settings/production.py
@@ -8,3 +8,7 @@ ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
