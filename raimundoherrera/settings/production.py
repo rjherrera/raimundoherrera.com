b4c2111a5e2f10fa495bc1db070c42a1211ec20b
Raimundo Herrera <rjherrera@uc.cl>
2021-02-07 00:12:39 -0300

    feat(hosts): allow hosts from env var

--- raimundoherrera/settings/production.py
+++ raimundoherrera/settings/production.py
@@ -3,10 +3,7 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'raimundoherrera.com',
-    'www.raimundoherrera.com',
-]
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
