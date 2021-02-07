c3e050281c8276304a124b286912c8b9ee28de58
Raimundo Herrera <rjherrera@uc.cl>
2021-02-06 22:20:45 -0300

    feat(home): add Link model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+  url = models.URLField()
+  title = models.CharField(max_length=200)
+  description = models.TextField(blank=True, default='')
+
+  def __str__(self):
+      return self.title
